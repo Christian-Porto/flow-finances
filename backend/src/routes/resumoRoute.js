0c1dc42777de5f48a42d3df4a21379667e26dc1e
Christian Porto <christianviking33@gmail.com>
2024-05-31 12:06:41 -0300

    Revert "commit base"
    This reverts commit c7e8bbd5816eea121e99a9a680cf07e2f742d872.

--- backend/src/routes/resumoRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import ResumoController from '../controllers/resumoController.js';
-
-const router = Router();
-
-router.get('/resumo/:ano/:mes', ResumoController.listarResumo);
-
-export default router;
--- frontend/src/app/models/despesa.ts
+++
@@ -1,5 +0,0 @@
-export class Despesa {
-
-  constructor(public descricao: string, public valor: number, public data?: Date, public id?: string) { }
-
-}
